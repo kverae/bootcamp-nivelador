b098aac091f810297ac373ec842c120e4fe05a8e
Katherine Vera <katherine.vera94@gmail.com>
2023-05-05 22:26:02 -0400

    api para filtrar usuarios por ciertos campos

--- server_solution.js
+++ server_solution.js
@@ -41,5 +41,19 @@ app.get('/all-users', async function (req, res) {
     }  
 });
 
+/**
+ * Definir una ruta que, mediante un GET, devuelva sólo los documentos
+ * que cumplen la condición en base a un query sobre uno o varios campos 
+ * de un documento.
+ */
+app.get('/filter-users', async function (req, res) {
+    try {
+        const users = await User.find(req.body);
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }  
+});
+
 app.listen(port, hostname);
 console.log(`Running on http://${hostname}:${port}`);
